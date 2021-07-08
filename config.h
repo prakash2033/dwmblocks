32bddee52dd738636ea16d9faf46e079bf70fc40
Prakash Vishwakarma <prakash2033@outlook.com>
2021-07-08 22:04:14 +0200

    Disabled sb-mailbox

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ static const Block blocks[] = {
 	/*{"",	"sb-cpu",		10,	18},*/
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	{"",	"sb-forecast",	18000,	5},
-	{"",	"sb-mailbox",	180,	12},
+	/*{"",	"sb-mailbox",	180,	12},*/
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
